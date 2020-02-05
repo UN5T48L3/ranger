d63f4a34b3c6980e6f570377f5e3cdd8ab8a0fd6
UN5T48L3 <39417235+UN5T48L3@users.noreply.github.com>
2020-02-05 14:34:01 +0300

    updated
    colors

--- ranger.sh
+++ ranger.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo -e ""
-echo -e "\e[32m#################################################################"
-echo -e "\e[32m# \e[31m        Free Open Source List Maker By UN5T48L3               \e[32m#"
-echo -e "\e[32m# \e[31mhttps://ipinfo.io/countries - you can find bunch of ASnumbers \e[32m#"
-echo -e "\e[32m#################################################################"
+echo -e "\e[32m#################################################################\e[0m"
+echo -e "\e[32m# \e[31m        Free Open Source List Maker By UN5T48L3               #\e[0m"
+echo -e "\e[32m# \e[31mhttps://ipinfo.io/countries - you can find bunch of ASnumbers #\e[0m"
+echo -e "\e[32m#################################################################\e[0m"
 
 while read asn; do
 	whois -h whois.radb.net -- "-i origin ${1:-$asn}" | grep -Eo "([0-9.]+){4}/[0-9]+" >> ranges.lst
 	echo -e "$asn <-- added to list"
 done < asn.txt
 echo -e ""
-echo -e "\e[34mAll ip ranges saved as ranges.lst\e[34m"
+echo -e "\e[34mAll ip ranges saved as ranges.lst\e[0m"
